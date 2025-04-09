54f6fbeb6c5f79bb945e580088a070bf9af099ba
Luke Miller <millerluke1364@gmail.com>
2025-04-09 05:34:42 +0000

    added separate thread for knowledge processing pipeline, added real time slack event handlers

--- slack_sensor_node/config.py
+++ slack_sensor_node/config.py
@@ -10,7 +10,8 @@ URL = f"http://{HOST}:{PORT}/koi-net"
 FIRST_CONTACT = "http://127.0.0.1:8000/koi-net"
 
 OBSERVING_CHANNELS = [
-    "C0593RJJ2CW"
+    "C0593RJJ2CW", #koi-research
+    "C082YTFAA83"  #koi-sensor-testing
 ]
 
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
