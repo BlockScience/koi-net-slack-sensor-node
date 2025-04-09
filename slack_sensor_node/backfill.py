e643ca3c2226f6fabf3b4317b543a6c90d73148d
lukvmil <millerluke1364@gmail.com>
2025-04-09 15:45:32 +0800

    implemented last processed ts state saving, backfill as part of server startup

--- slack_sensor_node/backfill.py
+++ slack_sensor_node/backfill.py
@@ -1,10 +1,13 @@
 import asyncio
+import logging
 from slack_sdk.errors import SlackApiError
 from rid_lib.ext import Bundle
 from rid_lib.types import SlackMessage
 from .core import node, slack_app
-from .config import OBSERVING_CHANNELS
+from .config import LAST_PROCESSED_TS, OBSERVING_CHANNELS
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 async def auto_retry(function, **params):
     try:
@@ -12,19 +15,24 @@ async def auto_retry(function, **params):
     except SlackApiError as e:
         if e.response["error"] == "ratelimited":
             retry_after = int(e.response.headers["Retry-After"])
-            print(f"timed out, waiting {retry_after} seconds")
+            logger.info(f"timed out, waiting {retry_after} seconds")
             await asyncio.sleep(retry_after)
             return await function(**params)
         else:
-            print("unknown error", e)
+            logger.warning("unknown error", e)
             quit()
 
-async def backfill_messages(channel_ids: list[str] = [], after=0):
+async def backfill_messages(
+    channel_ids: list[str] = [], 
+    after: int = 0
+):
     resp = await slack_app.client.team_info()
     team = resp.data["team"]
     team_id = team["id"]
 
     channels = [{"id": cid} for cid in channel_ids]
+    
+    logger.info("Scanning for channels")
     
     # get list of channels
     channel_cursor = None
@@ -32,12 +40,14 @@ async def backfill_messages(channel_ids: list[str] = [], after=0):
         resp = await slack_app.client.conversations_list(cursor=channel_cursor)
         result = resp.data
         channels.extend(result["channels"])
+        logger.info(f"Found {len(result['channels'])} channels")
         channel_cursor = result.get("response_metadata", {}).get("next_cursor")
 
+    logger.info(f"Scanning {len(channels)} channels for messages")
     for channel in channels:
         channel_id = channel["id"]
         
-        print(channel_id)
+        logger.info(f"Scanning {channel_id}...")
 
         # get list of messages in channel
         message_cursor = None
@@ -53,12 +63,14 @@ async def backfill_messages(channel_ids: list[str] = [], after=0):
             if not result["messages"]: break
             
             messages.extend(result["messages"])
+            logger.info(f"Found {len(result['messages'])} messages")
             if result["has_more"]:
                 message_cursor = result["response_metadata"]["next_cursor"]
             else:
                 message_cursor = None
 
-
+        logger.info(f"Scanning {len(messages)} messages")
+        messages.reverse()
         for message in messages:
             message_rid = SlackMessage(team_id, channel_id, message["ts"])
             
@@ -68,7 +80,7 @@ async def backfill_messages(channel_ids: list[str] = [], after=0):
                     rid=message_rid,
                     contents=message
                 )
-                
+                logger.info(f"{message_rid}")
                 node.processor.handle(bundle=message_bundle)                
             
             thread_ts = message.get("thread_ts")
@@ -95,7 +107,7 @@ async def backfill_messages(channel_ids: list[str] = [], after=0):
                     else:
                         threaded_message_cursor = None
                         
-                print(message_rid, "thread with", len(threaded_messages), "messages")
+                logger.info(f"{message_rid} thread with {len(threaded_messages)} messages")
                 
                 # don't double count thread parent message
                 for threaded_message in threaded_messages[1:]:
@@ -110,16 +122,19 @@ async def backfill_messages(channel_ids: list[str] = [], after=0):
                             contents=threaded_message
                         )
                         
+                        logger.info(f"{threaded_message_rid}")
                         node.processor.handle(bundle=threaded_message_bundle)     
 
-    print("done")
-    node.processor.flush_kobj_queue()
+    logger.info("done")
                         
 if __name__ == "__main__":    
     node.start()
     
     asyncio.run(
         backfill_messages(
-            channel_ids=OBSERVING_CHANNELS))
+            channel_ids=OBSERVING_CHANNELS,
+            after=LAST_PROCESSED_TS
+        )
+    )
     
-    node.processor.kobj_queue.join()
+    node.stop()
--- slack_sensor_node/handlers.py
+++ slack_sensor_node/handlers.py
@@ -1,4 +1,5 @@
 import logging
+import json
 from koi_net.processor.handler import HandlerType
 from koi_net.processor.knowledge_object import KnowledgeSource, KnowledgeObject
 from koi_net.processor.interface import ProcessorInterface
@@ -6,7 +7,8 @@ from koi_net.protocol.event import EventType
 from koi_net.protocol.edge import EdgeType
 from koi_net.protocol.node import NodeProfile
 from koi_net.protocol.helpers import generate_edge_bundle
-from rid_lib.types import KoiNetNode
+from rid_lib.types import KoiNetNode, SlackMessage
+from .config import LAST_PROCESSED_TS
 from .core import node
 
 logger = logging.getLogger(__name__)
@@ -50,3 +52,19 @@ def coordinator_contact(processor: ProcessorInterface, kobj: KnowledgeObject):
         # will fetch remotely instead of checking local cache
         processor.handle(rid=rid, source=KnowledgeSource.External)
     logger.info("Done")
+    
+@node.processor.register_handler(HandlerType.RID, rid_types=[SlackMessage])
+def update_last_processed_ts(processor: ProcessorInterface, kobj: KnowledgeObject):
+    msg_rid: SlackMessage = kobj.rid
+    ts = float(msg_rid.ts)
+    
+    global LAST_PROCESSED_TS
+    if ts < LAST_PROCESSED_TS: 
+        logger.warning(f"{ts} smaller")
+        return
+    
+    logger.warning(f"{ts} larger")
+    LAST_PROCESSED_TS = ts
+    
+    with open("state.json", "w") as f:
+        json.dump({"last_processed_ts": LAST_PROCESSED_TS}, f)
