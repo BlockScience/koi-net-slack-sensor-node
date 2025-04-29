1acf03726801221e8afe27c6e4a3fb1849401422
lukvmil <millerluke1364@gmail.com>
2025-04-29 21:52:34 +0800

    update uvicorn call

--- node/__main__.py
+++ node/__main__.py
@@ -2,7 +2,7 @@ import uvicorn
 from .core import node
 
 uvicorn.run(
-    app="slack_sensor_node.server:app",
+    app="node.server:app",
     host=node.config.server.host, 
     port=node.config.server.port
 )
