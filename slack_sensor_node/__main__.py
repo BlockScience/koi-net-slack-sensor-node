e9b00895f379c8da63d1e4032ee83584ca2f199c
Luke Miller <millerluke1364@gmail.com>
2025-05-15 09:29:16 +0000

    fixed uvicorn path

--- slack_sensor_node/__main__.py
+++ slack_sensor_node/__main__.py
@@ -2,7 +2,7 @@ import uvicorn
 from .core import node
 
 uvicorn.run(
-    app="node.server:app",
+    app="slack_sensor_node.server:app",
     host=node.config.server.host, 
     port=node.config.server.port
 )
