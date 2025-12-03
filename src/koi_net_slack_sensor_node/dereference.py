23c98bd325f597c11e446b65bf5124c817a5084f
lukvmil <millerluke1364@gmail.com>
2025-12-03 13:54:08 -0500

    refactored unused dereference functions into dereferencer component. still unused, at some point the koi-net effector needs to be overhauled.

--- src/koi_net_slack_sensor_node/dereference.py
+++ src/koi_net_slack_sensor_node/dereference.py
@@ -2,59 +2,63 @@ from rid_lib import RID
 from rid_lib.types import SlackMessage, SlackChannel, SlackUser, SlackWorkspace
 from rid_lib.ext import Bundle
 from koi_net.protocol.api_models import BundlesPayload
-from .core import node, slack_app
 
-async def fetch_missing(payload: BundlesPayload):
-    found_bundles: list[Bundle] = []
-    for rid in payload.not_found:
-        if type(rid) not in (
-            SlackMessage, SlackChannel, SlackUser, SlackWorkspace
-        ): continue
-        
-        bundle = await dereference(rid)
-        if bundle:
-            found_bundles.append(bundle)
-    
-    for bundle in found_bundles:
-        payload.not_found.remove(bundle.rid)
-        payload.bundles.append(bundle)
-    
-    return payload
 
-async def dereference(rid: RID):
-    if type(rid) == SlackMessage:
-        resp = await slack_app.client.conversations_replies(
-            channel=rid.channel_id,
-            ts=rid.ts
-        )
-        message = resp["messages"][0]
+class Dereferencer:
+    def __init__(self, slack_app):
+        self.slack_app = slack_app
         
-        return Bundle.generate(rid, message)
+    async def fetch_missing(self, payload: BundlesPayload):
+        found_bundles: list[Bundle] = []
+        for rid in payload.not_found:
+            if type(rid) not in (
+                SlackMessage, SlackChannel, SlackUser, SlackWorkspace
+            ): continue
+            
+            bundle = await self.dereference(rid)
+            if bundle:
+                found_bundles.append(bundle)
         
-    elif type(rid) == SlackChannel:
-        resp = await slack_app.client.conversations_info(
-            channel=rid.channel_id
-        )
-        channel = resp["channel"]
+        for bundle in found_bundles:
+            payload.not_found.remove(bundle.rid)
+            payload.bundles.append(bundle)
         
-        return Bundle.generate(rid, channel)
+        return payload
+
+    async def dereference(self, rid: RID):
+        if type(rid) == SlackMessage:
+            resp = await self.slack_app.client.conversations_replies(
+                channel=rid.channel_id,
+                ts=rid.ts
+            )
+            message = resp["messages"][0]
+            
+            return Bundle.generate(rid, message)
+            
+        elif type(rid) == SlackChannel:
+            resp = await self.slack_app.client.conversations_info(
+                channel=rid.channel_id
+            )
+            channel = resp["channel"]
+            
+            return Bundle.generate(rid, channel)
+            
+        elif type(rid) == SlackUser:
+            profile_resp = await self.slack_app.client.users_profile_get(user=rid.user_id)
+            profile = profile_resp["profile"]
+            
+            user_resp = await self.slack_app.client.users_info(user=rid.user_id)
+            user = user_resp["user"]
+            
+            user["profile"] = profile
+            
+            return Bundle.generate(rid, user)
         
-    elif type(rid) == SlackUser:
-        profile_resp = await slack_app.client.users_profile_get(user=rid.user_id)
-        profile = profile_resp["profile"]
+        elif type(rid) == SlackWorkspace:
+            resp = await self.slack_app.client.team_info(team=rid.team_id)
+            workspace = resp["team"]
+            
+            return Bundle.generate(rid, workspace)
         
-        user_resp = await slack_app.client.users_info(user=rid.user_id)
-        user = user_resp["user"]
-        
-        user["profile"] = profile
-        
-        return Bundle.generate(rid, user)
-    
-    elif type(rid) == SlackWorkspace:
-        resp = await slack_app.client.team_info(team=rid.team_id)
-        workspace = resp["team"]
-        
-        return Bundle.generate(rid, workspace)
-    
-    else:
-        raise TypeError(f"RID of type {type(rid)!r} is not allowed")
+        else:
+            raise TypeError(f"RID of type {type(rid)!r} is not allowed")
